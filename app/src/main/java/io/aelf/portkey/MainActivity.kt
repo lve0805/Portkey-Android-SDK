46dbe664b843335d0472df4b031ba3ad85685d03
Carbon.lv <130360750+carbon-portkey@users.noreply.github.com>
2023-08-30 20:27:28 +0800

    fix: guardian controller view issue

--- app/src/main/java/io/aelf/portkey/MainActivity.kt
+++ app/src/main/java/io/aelf/portkey/MainActivity.kt
@@ -10,11 +10,29 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricPrompt
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
@@ -31,7 +49,6 @@ import io.aelf.portkey.ui.basic.Toast
 import io.aelf.portkey.ui.button.ButtonConfig
 import io.aelf.portkey.ui.button.HugeButton
 import io.aelf.portkey.utils.log.GLogger
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -114,9 +131,62 @@ class MainActivity : FragmentActivity() {
                     }
                 }
             )
+            ServiceEnvironment()
         }
         PortkeySDKViewStub()
     }
+
+    @Composable
+    private fun ServiceEnvironment() {
+        val context = LocalContext.current
+        val data = mutableListOf(
+            "MainNet",
+            "TestNet",
+            "Text1",
+            "Test2"
+        )
+        var expand by remember {
+            mutableStateOf(false)
+        }
+        Box(
+            modifier = Modifier
+                .height(50.dp)
+                .width(200.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color(0xFF4285F4))
+                .padding(vertical = 10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Select Environment", modifier = Modifier.clickable {
+                expand = !expand
+            }, color = Color.White)
+            DropdownMenu(
+                expanded = expand,
+                onDismissRequest = { expand = false },
+                modifier = Modifier.width(100.dp)
+            ) {
+                data.forEach {
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = it, modifier = Modifier.padding(start = 10.dp))
+                        },
+                        onClick = {
+                            RetrofitProvider.resetOrInitMainRetrofit(
+                                when (it) {
+                                    "MainNet" -> "https://did-portkey.portkey.finance"
+                                    "TestNet" -> "https://did-portkey-test.portkey.finance"
+                                    "Text1" -> "https://testnet-applesign.portkey.finance"
+                                    "Test2" -> "https://testnet-applesign2.portkey.finance"
+                                    else -> "https://testnet-applesign.portkey.finance"
+                                }
+                            )
+                            Toast.showToast(context, "Init with:$it")
+                        })
+                }
+            }
+        }
+    }
+
 
     internal fun initDebug(context: Context) {
         InitProcessor.init(SDkInitConfig.Builder().build(), context)
